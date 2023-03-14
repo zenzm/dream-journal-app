44ce2d222ba75551b0d50ac97d271228c4095c1c
Madeline Zenz <zenzm@students.westerntc.edu>
2023-03-13 23:08:35 -0500

    Initial commit

---
+++ app/src/main/java/com/example/dreamjournal/models/Dream.java
@@ -0,0 +1,41 @@
+package com.example.dreamjournal.models;
+
+import java.util.Date;
+
+public class Dream {
+
+    private int id;
+    private Date date;
+    private String description;
+
+    public Dream(int id, Date date, String description) {
+        this.id = id;
+        this.date = date;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
+
---
+++ app/src/main/java/com/example/dreamjournal/models/Symbol.java
@@ -0,0 +1,39 @@
+package com.example.dreamjournal.models;
+
+public class Symbol {
+
+    private int id;
+    private String name;
+    private String description;
+
+    public Symbol(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
+
